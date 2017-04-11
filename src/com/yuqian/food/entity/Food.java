eb632772990fec35ba4533ebd9f975a9e32bf4cf
yangy <yangyuan@imudges.com>
2017-04-11 18:05:34 +0800

    food表建立

---
+++ src/com/yuqian/food/entity/Food.java
@@ -0,0 +1,64 @@
+package com.yuqian.food.entity;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Table;
+
+/**
+ * Created by yangy on 2017/4/11.
+ */
+@Table("food")
+public class Food {
+    @Column
+    private int id;
+    @Column
+    private String foodName;
+
+    @Column
+    private String foodPrice;
+
+    @Column
+    private String foodDescribe;
+
+    @Column
+    private String foodPhotoUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(String foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public String getFoodDescribe() {
+        return foodDescribe;
+    }
+
+    public void setFoodDescribe(String foodDescribe) {
+        this.foodDescribe = foodDescribe;
+    }
+
+    public String getFoodPhotoUrl() {
+        return foodPhotoUrl;
+    }
+
+    public void setFoodPhotoUrl(String foodPhotoUrl) {
+        this.foodPhotoUrl = foodPhotoUrl;
+    }
+}
