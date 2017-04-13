4c92f644f709a23e3c88c7c7a1cb2a7db1d7413e
yangy <yangyuan@imudges.com>
2017-04-13 10:22:18 +0800

    添加菜品功能

--- src/com/yuqian/food/controller/FoodModule.java
+++ src/com/yuqian/food/controller/FoodModule.java
@@ -1,16 +1,18 @@
 package com.yuqian.food.controller;
 
 import com.yuqian.food.entity.Food;
+import com.yuqian.food.util.MD5;
+import com.yuqian.food.util.QiNiuUpPhotoUtil;
 import org.nutz.dao.Dao;
 import org.nutz.dao.QueryResult;
 import org.nutz.dao.pager.Pager;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Fail;
-import org.nutz.mvc.annotation.GET;
-import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.*;
+import org.nutz.mvc.upload.TempFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -43,6 +45,50 @@ public class FoodModule {
         hashMap.put("foods",queryResult.getList());
 
         return hashMap;
+    }
+    @At("addFood")
+    @Ok("json")
+    @Fail("http:500")
+    @POST
+    public Object addFood(@Param("foodName")String foodName,
+                          @Param("foodPrice")String foodPrice,
+                          @Param("foodDescribe")String foodDescribe,
+                          @Param("foodPictureFile")TempFile foodPictureFile){
+        HashMap<String,Object> hashMap=new HashMap<>();
+        if(foodName==null||"".equals(foodName)||foodPrice==null||"".equals(foodPrice)
+                ||foodDescribe==null||"".equals(foodDescribe)) {
+            hashMap.put("msg","参数错误");
+            hashMap.put("state",0);
+            return hashMap;
+        }
+        File file=foodPictureFile.getFile();
+        final String urlhead = "http://oe1rqbymq.bkt.clouddn.com/";
+        String keyString= MD5.encryptTimeStamp(System.currentTimeMillis()+"_"+foodName);
+        String photoUrl=urlhead+keyString;
+        QiNiuUpPhotoUtil qiNiuUpPhotoUtil=new QiNiuUpPhotoUtil();
+        if (qiNiuUpPhotoUtil.upload(file, keyString)) {
+            hashMap.put("msg","添加成功");
+            hashMap.put("state",1);
+            Food food=new Food();
+            food.setFoodDescribe(foodDescribe);
+            food.setFoodName(foodName);
+            food.setFoodPhotoUrl(photoUrl);
+            food.setFoodPrice(foodPrice);
+            dao.insert(food);
+            try {
+                foodPictureFile.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+                hashMap.put("msg","失败");
+                hashMap.put("state",0);
+                return hashMap;
+            }
+            return hashMap;
+        }else {
+            hashMap.put("msg","图片文件上传失败");
+            hashMap.put("state",0);
+            return hashMap;
+        }
     }
 
 }
